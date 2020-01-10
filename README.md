77448c453adbb4e246738d09fb64df3d6f993aec
alejandro castellanos <alejandrocastellanos0827@gmail.com>
2020-01-10 00:11:20 -0500

    se termina prueba, pendiente publicar en servidor gratuito

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # apibiblioteca
 Api que exponé URLs para consultar, registrar y eliminar datos sobre libros.
+
+# Url
+  - http://
+
+# Enpoints
+  - /registrar-google-libro
+  - /registrar-itbook-libro
+  - /eliminar-libro
+  - /buscar-libro-db-google
+  - /buscar-libro-db-itbook
+
+# Headers
+  - X-Api-Key 1234abcd
+  - Content-Type application/json
+
+# Metodos y Json por Endpoint
+```sh
+endpoint = /registrar-google-libro
+json = {"id":"id libro google"} (ejemplo: {"id":"9781484206485"})
+method = POST
+```
+
+```sh
+endpoint = /registrar-itbook-libro
+json = {"id":"id libro google"} (ejemplo: {"id":"9781484206485"})
+method = POST
+```
+
+```sh
+endpoint = /eliminar-libro
+json = {"id":"id de la bd"} (ejemplo: {"id":"6"})
+method = DELETE
+```
+
+```sh
+endpoint = /buscar-libro-db-google
+json = {"search":"buscar libro por cualquier atributo"} (ejemplo: {"search":"Python 3"})
+method = POST
+```
+
+```sh
+endpoint = /buscar-libro-db-itbook
+json = {"search":"buscar libro por cualquier atributo"} (ejemplo: {"search":"javascript"})
+method = POST
+```
--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import requests
 import json
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
 from services import ServiceGoogle, ServiceItbook, ServiceItbookall, ServiceGoogleall
 
+password = '1234abcd'
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1602@localhost/biblioteca'
 api = Api(app)
@@ -28,92 +29,118 @@ class Book(db.Model):
 
 def SearchBook(search):
     return Book.query.filter((Book.titulo==search) | (Book.subtitulo==search) | (Book.autor==search) | (Book.categoria==search) | (Book.fecha_publicacion==search) | (Book.editor==search) | (Book.descripcion==search) | (Book.imagen==search)).all()
+
 #crear libro de api google por ID
 class RegisterGoogleBook(Resource):
     def post(self):
-        data_json = request.json
-        try:
-            id = data_json["id"]
-            servicegoogle = ServiceGoogle(id)
-            registergooglebook = Book(titulo=servicegoogle['titulo'], subtitulo=servicegoogle['subtitulo'], autor=servicegoogle['autor'], categoria=servicegoogle['categoria'], fecha_publicacion=servicegoogle['fecha_publicacion'], editor=servicegoogle['editor'], descripcion=servicegoogle['descripcion'], imagen=servicegoogle['imagen'])
-            db.session.add(registergooglebook)
-            db.session.commit()    
-            result = {"Resultado":"Creado Correctamente", "id":registergooglebook.id}         
-        except Exception as e:
-            result = {"Resultado":"Datos incorrectos", "Descripcion":str(e)}
-        return result
+        headers = request.headers
+        auth = headers.get("X-Api-Key")
+        if auth == password:
+            data_json = request.json
+            try:
+                id = data_json["id"]
+                servicegoogle = ServiceGoogle(id)
+                registergooglebook = Book(titulo=servicegoogle['titulo'], subtitulo=servicegoogle['subtitulo'], autor=servicegoogle['autor'], categoria=servicegoogle['categoria'], fecha_publicacion=servicegoogle['fecha_publicacion'], editor=servicegoogle['editor'], descripcion=servicegoogle['descripcion'], imagen=servicegoogle['imagen'])
+                db.session.add(registergooglebook)
+                db.session.commit()    
+                result = {"Resultado":"Creado Correctamente", "id":registergooglebook.id}         
+            except Exception as e:
+                result = {"Resultado":"Datos incorrectos", "Descripcion":str(e)}
+            return result
+        else:
+            return {"message": "ERROR: Unauthorized"}, 401
 
 #crear libro de api libros de programacion por ID
 class RegisterItbookBook(Resource):
     def post(self):
-        data_json = request.json
-        try:
-            id = data_json["id"]
-            serviceitbook = ServiceItbook(id)
-            registeritbookbook = Book(titulo=serviceitbook['titulo'], subtitulo=serviceitbook['subtitulo'], autor=serviceitbook['autor'], categoria=serviceitbook['categoria'], fecha_publicacion=serviceitbook['fecha_publicacion'], editor=serviceitbook['editor'], descripcion=serviceitbook['descripcion'], imagen=serviceitbook['imagen'])
-            db.session.add(registeritbookbook)
-            db.session.commit()
+        headers = request.headers
+        auth = headers.get("X-Api-Key")
+        if auth == password:
+            data_json = request.json
+            try:
+                id = data_json["id"]
+                serviceitbook = ServiceItbook(id)
+                registeritbookbook = Book(titulo=serviceitbook['titulo'], subtitulo=serviceitbook['subtitulo'], autor=serviceitbook['autor'], categoria=serviceitbook['categoria'], fecha_publicacion=serviceitbook['fecha_publicacion'], editor=serviceitbook['editor'], descripcion=serviceitbook['descripcion'], imagen=serviceitbook['imagen'])
+                db.session.add(registeritbookbook)
+                db.session.commit()
 
-            result = {"Resultado":"Creado Correctamente", "Id":registeritbookbook.id}
-        except Exception as e:
-            result = {"Resultado":"Datos incorrectos", "Descripcion":str(e)}
-        return result
+                result = {"Resultado":"Creado Correctamente", "Id":registeritbookbook.id}
+            except Exception as e:
+                result = {"Resultado":"Datos incorrectos", "Descripcion":str(e)}
+            return result
+        else:
+            return {"message": "ERROR: Unauthorized"}, 401
 
 #eliminar libro
 class DeleteBook(Resource):
     def delete(delf):
-        data_json = request.json
-        try:
-            id = data_json["id"]
-            deletebook = db.session.query(Book).filter(Book.id==int(id)).first()
-            result = {"Resultado":"El id no existe"}
-            if deletebook != None:
-                db.session.query(Book).filter(Book.id==int(id)).delete()
-                db.session.commit()
-                result = {"Resultado":"Eliminado Correctamente"}
-        except Exception as e:
-            result = {"Resultado":"Datos incorrectos", "Descripcion":str(e)}
-        return result
+        headers = request.headers
+        auth = headers.get("X-Api-Key")
+        if auth == password:
+            data_json = request.json
+            try:
+                id = data_json["id"]
+                deletebook = db.session.query(Book).filter(Book.id==int(id)).first()
+                result = {"Resultado":"El id no existe"}
+                if deletebook != None:
+                    db.session.query(Book).filter(Book.id==int(id)).delete()
+                    db.session.commit()
+                    result = {"Resultado":"Eliminado Correctamente"}
+            except Exception as e:
+                result = {"Resultado":"Datos incorrectos", "Descripcion":str(e)}
+            return result
+        else:
+            return {"message": "ERROR: Unauthorized"}, 401
 
 #buscar libro en db y en google books
 class SearchBookDbYGoogle(Resource):
     def post(delf):
-        data_json = request.json
-        try:
-            search = data_json["search"] 
-            queryfilter = SearchBook(search)
-            if len(queryfilter) > 0:
-                resultbooks = []
-                for x in range(0, len(queryfilter)-1):
-                    book = {"id":queryfilter[x].id, "titulo":queryfilter[x].titulo, "subtitulo":queryfilter[x].subtitulo, "autor":queryfilter[x].autor, "categoria":queryfilter[x].categoria, "fecha_publicacion":queryfilter[x].fecha_publicacion, "editor":queryfilter[x].editor, "descripcion":queryfilter[x].descripcion, "imagen":queryfilter[x].imagen}
-                    resultbooks.append(book)
-                result = {"Resultado": resultbooks}
-            else:
-                searchGoogleBooks = ServiceGoogleall(search)
-                result = {"Resultado": searchGoogleBooks}
-        except Exception as e:
-            result = {"Resultado":"Error", "Descripcion":str(e)}
-        return result
+        headers = request.headers
+        auth = headers.get("X-Api-Key")
+        if auth == password:
+            data_json = request.json
+            try:
+                search = data_json["search"] 
+                queryfilter = SearchBook(search)
+                if len(queryfilter) > 0:
+                    resultbooks = []
+                    for x in range(0, len(queryfilter)-1):
+                        book = {"id":queryfilter[x].id, "titulo":queryfilter[x].titulo, "subtitulo":queryfilter[x].subtitulo, "autor":queryfilter[x].autor, "categoria":queryfilter[x].categoria, "fecha_publicacion":queryfilter[x].fecha_publicacion, "editor":queryfilter[x].editor, "descripcion":queryfilter[x].descripcion, "imagen":queryfilter[x].imagen}
+                        resultbooks.append(book)
+                    result = {"Resultado": resultbooks}
+                else:
+                    searchGoogleBooks = ServiceGoogleall(search)
+                    result = {"Resultado": searchGoogleBooks}
+            except Exception as e:
+                result = {"Resultado":"Error", "Descripcion":str(e)}
+            return result
+        else:
+            return {"message": "ERROR: Unauthorized"}, 401
 
 #buscar libro en db y en itbooks
 class SearchBookDbYItbook(Resource):
     def post(delf):
-        data_json = request.json
-        try:
-            search = data_json["search"] 
-            queryfilter = SearchBook(search)
-            if len(queryfilter) > 0:
-                resultbooks = []
-                for x in range(0, len(queryfilter)-1):
-                    book = {"id":queryfilter[x].id, "titulo":queryfilter[x].titulo, "subtitulo":queryfilter[x].subtitulo, "autor":queryfilter[x].autor, "categoria":queryfilter[x].categoria, "fecha_publicacion":queryfilter[x].fecha_publicacion, "editor":queryfilter[x].editor, "descripcion":queryfilter[x].descripcion, "imagen":queryfilter[x].imagen}
-                    resultbooks.append(book)
-                result = {"Resultado": resultbooks}
-            else:
-                searchItBooks = ServiceItbookall(search)
-                result = {"Resultado": searchItBooks}
-        except Exception as e:
-            result = {"Resultado":"Error", "Descripcion":str(e)}
-        return result
+        headers = request.headers
+        auth = headers.get("X-Api-Key")
+        if auth == password:
+            data_json = request.json
+            try:
+                search = data_json["search"] 
+                queryfilter = SearchBook(search)
+                if len(queryfilter) > 0:
+                    resultbooks = []
+                    for x in range(0, len(queryfilter)-1):
+                        book = {"id":queryfilter[x].id, "titulo":queryfilter[x].titulo, "subtitulo":queryfilter[x].subtitulo, "autor":queryfilter[x].autor, "categoria":queryfilter[x].categoria, "fecha_publicacion":queryfilter[x].fecha_publicacion, "editor":queryfilter[x].editor, "descripcion":queryfilter[x].descripcion, "imagen":queryfilter[x].imagen}
+                        resultbooks.append(book)
+                    result = {"Resultado": resultbooks}
+                else:
+                    searchItBooks = ServiceItbookall(search)
+                    result = {"Resultado": searchItBooks}
+            except Exception as e:
+                result = {"Resultado":"Error", "Descripcion":str(e)}
+            return result, 200
+        else:
+            return {"message": "ERROR: Unauthorized"}, 401
 
 api.add_resource(RegisterGoogleBook, '/registrar-google-libro')   
 api.add_resource(RegisterItbookBook, '/registrar-itbook-libro')
